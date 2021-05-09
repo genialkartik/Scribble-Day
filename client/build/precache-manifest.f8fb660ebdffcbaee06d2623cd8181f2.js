95f88f2d616ab1b1bd0b6900aa480aef55f52d54
Kartik Tyagi <kartik9756@gmail.com>
2021-05-09 16:08:56 +0530

    env fix

---
+++ client/build/precache-manifest.f8fb660ebdffcbaee06d2623cd8181f2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e524608d394dc465aeba656217d263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac7e2bec5f47a981448d",
+    "url": "/static/css/2.9a766daa.chunk.css"
+  },
+  {
+    "revision": "62f385b8dbf208ff5147",
+    "url": "/static/css/main.c70c540f.chunk.css"
+  },
+  {
+    "revision": "ac7e2bec5f47a981448d",
+    "url": "/static/js/2.37238ae2.chunk.js"
+  },
+  {
+    "revision": "48ac8ec662c878179d9a66a3836a4b97",
+    "url": "/static/js/2.37238ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f385b8dbf208ff5147",
+    "url": "/static/js/main.901f00de.chunk.js"
+  },
+  {
+    "revision": "619b3b2a0eb2d8191259",
+    "url": "/static/js/runtime-main.f6673272.js"
+  },
+  {
+    "revision": "241eefb3bd606dcb4a12e8b672caf3b0",
+    "url": "/static/media/ScribbleDay.241eefb3.png"
+  },
+  {
+    "revision": "ea18e75f1d045af5ce52983868cb6258",
+    "url": "/static/media/announcement.ea18e75f.png"
+  },
+  {
+    "revision": "25f1372b285869fe60b3820b0ce60338",
+    "url": "/static/media/hack.25f1372b.jpg"
+  },
+  {
+    "revision": "5520076adcaf0a420dd379ddd7e2e429",
+    "url": "/static/media/maleback.5520076a.png"
+  },
+  {
+    "revision": "3b15893ea05d3dad6279415aaf3f7143",
+    "url": "/static/media/malefront.3b15893e.png"
+  },
+  {
+    "revision": "7ae50b19f79520ce33bca71ccac86d18",
+    "url": "/static/media/tshirt1.7ae50b19.png"
+  },
+  {
+    "revision": "7ae50b19f79520ce33bca71ccac86d18",
+    "url": "/static/media/tshirt2.7ae50b19.png"
+  }
+]);
