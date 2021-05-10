309fd6f1149428910057f16688d87a39fba0d387
Madan Lal <madanparmar637@gmail.com>
2021-05-10 17:15:50 +0530

    footer fixed

--- client/src/components/includes/footer.js
+++ client/src/components/includes/footer.js
@@ -4,17 +4,21 @@ import "./style.css";
 export default function Footer() {
   return (
     <div>
-      <footer className="footer-cont pb-5">
-        <div class="row bottom-part">
-          <div class="d-lg-flex justify-content-between bd-highlight col-md-12 mb-5 px-5">
-            <div class="p-2 align-self-center flex-fill bd-highlight">
-              <div class="fa fa-facebook px-2"></div>
-              <div class="fa fa-linkedin px-2"></div>
-              <div class="fa fa-twitter px-2"></div>
-              <div class="fa fa-instagram px-2"></div>
-            </div>
-            <div class="p-2 row flex-fill bd-highlight justify-content-left">
-              <div class="p-2 d-lg-flex">
+      <footer className="footer-cont">
+        <div className="row bottom-part justify-content-end">
+          
+          <div className="col-12 col-sm-10 col-md-8 text-right">
+              <div class="fa fa-mobile px-2 grey-text">
+                &nbsp;&nbsp;<span id="contact">888-777-666</span>
+              </div>
+              <div class="fa fa-envelope-o px-2 grey-text">
+                &nbsp;&nbsp;info@itcraft.in
+              </div>
+          </div>
+
+          <div className="col-12 col-sm-10 col-md-8 d-flex justify-content-end align-items-center">
+            <div class="footer-links">
+              <div class="d-flex">
                 <a href="https://ethicallearner.com/tnc">
                   <div class="p-2 flex-fill d-flex bd-highlight">Terms</div>
                 </a>
@@ -29,17 +33,17 @@ export default function Footer() {
                 </a>
               </div>
             </div>
-            <div class="p-2 align-self-center flex-fill bd-highlight">
-              <div class="fa fa-mobile px-2 grey-text">
-                &nbsp;&nbsp;<span id="contact">888-777-666</span>
-              </div>
-              <div class="fa fa-envelope-o px-2 grey-text">
-                &nbsp;&nbsp;info@itcraft.in
-              </div>
+            <div class="footer-socials">
+              <div class="fa fa-facebook px-2"></div>
+              <div class="fa fa-linkedin px-2"></div>
+              <div class="fa fa-twitter px-2"></div>
+              <div class="fa fa-instagram px-2"></div>
             </div>
           </div>
+
         </div>
       </footer>
+      
     </div>
   );
 }
